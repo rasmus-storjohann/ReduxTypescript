7e0d4bd2c0a11e0fe2a7e6abeb587f2b41aec979
Rasmus Storjohann <rasmus@storbot.ca>
2018-04-04 14:49:23 -0700

    Add a snapshot test

--- src/hello_world/__tests__/components.test.tsx
+++ src/hello_world/__tests__/components.test.tsx
@@ -1,11 +1,20 @@
 import 'react-native';
+import { Text } from 'react-native'
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Output } from '../components';
+import TestRenderer from 'react-test-renderer';
 
 it('output renders correctly', () => {
     const tree = renderer.create(
         <Output value={9} />
     );
     expect(tree).toBeDefined();
+});
+
+it('matches the snapshot', () => {
+    const tree = TestRenderer.create(
+        <Text>boa</Text>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
 });
